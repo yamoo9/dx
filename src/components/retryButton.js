305c2d41ff24f71a7041d7ad8122cb9882800c3c
yamoo9 <yamoog@naver.com>
2022-02-04 10:08:53 +0900

    web/react

---
+++ src/components/retryButton.js
@@ -0,0 +1,14 @@
+function RetryButton({ onRetry, isDisabled }) {
+
+  return React.createElement(
+    'button', 
+    { 
+      type: 'button',
+      className: 'retryButton',
+      disabled: isDisabled,
+      onClick: onRetry
+    }, 
+    '재실행'
+  );
+
+}
